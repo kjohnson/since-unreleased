c3a626e421222f50a79bb831f9ff0237fdbde303
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2021-02-12 17:01:37 -0500

    Exclude vendor and node_modules when replacing

--- bin/since-unreleased.sh
+++ bin/since-unreleased.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -not \( -path "./vendor/*" -o -path "./node_modules/*" \) -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+
