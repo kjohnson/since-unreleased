3d82973312df1d83dcc3e43723579237fce53ef7
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2021-02-12 17:02:15 -0500

    Add plain text description of the script.

--- bin/since-unreleased.sh
+++ bin/since-unreleased.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -not \( -path "./vendor/*" -o -path "./node_modules/*" \) -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+# Find files in the directory ./ (root)
+# find ./ -type f
 
+# Include files with extensions .php, .js, or .jsx
+# \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \)
+
+# Exclude the directories vendor (composer) and node_modules (npm)
+# -not \( -path "./vendor/*" -o -path "./node_modules/*" \)
+
+# Replace the string @unreleased with the value of the first argument, prefixed with @since
+# -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+
+find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -not \( -path "./vendor/*" -o -path "./node_modules/*" \) -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
