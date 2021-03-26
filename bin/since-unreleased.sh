6c3acae67cfddbaf045aa47fd46e08a3e102cdd1
Kyle B. Johnson <kyle.johnson@hey.com>
2021-03-26 09:29:08 -0400

    fix: Configure edit in place to not use backups.

--- bin/since-unreleased.sh
+++ bin/since-unreleased.sh
@@ -10,6 +10,7 @@
 # -not \( -path "./vendor/*" -o -path "./node_modules/*" \)
 
 # Replace the string @unreleased with the value of the first argument, prefixed with @since
-# -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+# @note `sed -Ei` Same as -E -i with no backup suffix - FILE will be edited in-place without creating a backup.
+# -exec sed -Ei "s/@unreleased/@since $1/g" {} \;
 
-find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -not \( -path "./vendor/*" -o -path "./node_modules/*" \) -exec sed -i -e "s/@unreleased/@since $1/g" {} \;
+find ./ -type f \( -name "*.php" -o -name "*.js" -o -name "*.jsx" \) -not \( -path "./vendor/*" -o -path "./node_modules/*" \) -exec sed -Ei "s/@unreleased/@since $1/g" {} \;
